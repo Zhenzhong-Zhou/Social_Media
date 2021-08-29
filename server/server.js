4d80e2a02e3bf8eb4ffda6f6b00b45fcd3b2a345
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-29 02:52:21 -0600

    Setup server, connect routes and database, add dependencies, and bugs fix

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/users.js";
+
+const app = express();
+dotenv.config();
+
+// Middlewares
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+
+app.use("/", indexRouter);
+app.use("/api/users", userRouter);
+
+const PORT = process.env.PORT;
+mongoose.connection.once("open", () => console.log("Connected to Mongoose..."));
+mongoose.connect(process.env.DATABASE_URL)
+	.then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+	.catch((error) => console.log(error));
