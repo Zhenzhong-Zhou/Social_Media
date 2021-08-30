32aad149e3fdb11ec23519e749f95406ea8529f3
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-29 19:56:27 -0600

    Add user operation routes: CRUD, follow and unfollow

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,3 +1,87 @@
-export const getUsers = async (req, res) => {
-	res.send("Welcome to users page!");
+import bcrypt from "bcrypt";
+import User from "../models/user.js";
+
+// UPDATE User Controller
+export const updateUser = async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		if (req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(12);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			} catch (error) {
+				res.status(500).json(error);
+			}
+		}
+		try {
+			await User.findByIdAndUpdate(req.params.id, {$set: req.body});
+			res.status(200).json("Account has been updated!");
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("Cannot update this account!");
+	}
+};
+// DELETE User Controller
+export const deleteUser = async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			await User.findByIdAndDelete(req.params.id);
+			res.status(200).json("Account has been deleted!");
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("Cannot delete this account!");
+	}
+}
+// FIND User Controller
+export const findUser = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const {password, updatedAt, ...other} = user._doc;
+		res.status(200).json(other);
+	} catch (error) {
+		res.status(500).json(error);
+	}
+};
+// FOLLOW User Controller
+export const followUser = async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (!user.followers.includes(req.body.userId)) {
+				await user.updateOne({$push: {followers: req.body.userId}});
+				await currentUser.updateOne({$push: {followings: req.params.id}});
+				res.status(200).json("User has been followed!");
+			} else {
+				res.status(403).json("Cannot follow the same follower again!");
+			}
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("Cannot follow yourself!");
+	}
+};
+// UNFOLLOW User Controller
+export const unfollowUser = async (req, res) => {
+	if (req.body.userId !== req.params.id) {
+		try {
+			const user = await User.findById(req.params.id);
+			const currentUser = await User.findById(req.body.userId);
+			if (user.followers.includes(req.body.userId)) {
+				await user.updateOne({$pull: {followers: req.body.userId}});
+				await currentUser.updateOne({$pull: {followings: req.params.id}});
+				res.status(200).json("User has been unfollowed!");
+			} else {
+				res.status(403).json("Cannot follow this user!");
+			}
+		} catch (error) {
+			res.status(500).json(error);
+		}
+	} else {
+		res.status(403).json("Cannot unfollow yourself!");
+	}
 };
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,16 @@
 import express from "express";
-import {getUsers} from "../controllers/users.js";
+import {updateUser, deleteUser, findUser, followUser, unfollowUser} from "../controllers/users.js";
 
 const router = express.Router();
-
-router.get("/", getUsers);
+// Update user route
+router.put("/:id", updateUser);
+// Delete user route
+router.delete("/:id", deleteUser);
+// Find user route
+router.get("/:id", findUser);
+// Follow user route
+router.put("/:id/follow", followUser);
+// Unfollow user route
+router.put("/:id/unfollow", unfollowUser);
 
 export default router;
