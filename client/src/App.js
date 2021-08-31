a84f040cd433cb5a1fd7622cc765da10f9f6a43e
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 05:29:57 -0600

    Shorten App.js imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import {Home, Login, Profile, Register} from "./pages";
 
 const App = () => {
 	return (
-		<Home/>
+		<>
+			<Home/>
+			<Profile/>
+			<Login/>
+			<Register/>
+		</>
 	);
 };
 
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Home} from "../pages/home/Home";
+export {default as Profile} from "../pages/profile/Profile";
+export {default as Login} from "../pages/login/Login";
+export {default as Register} from "../pages/register/Register";
