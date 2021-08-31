c44aaff64acd72a1459e4a873ae7d1393f66c8e3
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 15:15:58 -0600

    Add routes into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import {Home, Login, Profile, Register} from "./pages";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 const App = () => {
 	return (
-		<>
-			<Home/>
-			<Profile/>
-			<Login/>
-			<Register/>
-		</>
+		<BrowserRouter>
+			<Switch>
+				<Route path={"/"} exact>
+					<Home/>
+				</Route>
+				<Route path={"/login"} exact>
+					<Login/>
+				</Route>
+				<Route path={"/register"} exact>
+					<Register/>
+				</Route>
+				<Route path={"/profile/:username"} exact>
+					<Profile/>
+				</Route>
+			</Switch>
+		</BrowserRouter>
 	);
 };
 
