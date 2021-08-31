133692f5ed703e432c23d9036af5c6f9c186a9d7
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 05:13:56 -0600

    Add login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
 
 const App = () => {
 	return (
-		<Profile/>
+		<Login/>
 	);
 };
 
