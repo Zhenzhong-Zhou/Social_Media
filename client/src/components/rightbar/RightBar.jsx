ac8bce134271807f85ac48ceeb297be6dd7094eb
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 03:18:25 -0600

    Add rigtbar components

--- client/src/components/rightbar/RightBar.jsx
+++ client/src/components/rightbar/RightBar.jsx
@@ -3,7 +3,57 @@ import "./righbar.css";
 const RightBar = () => {
 	return (
 		<div className={"rightBar"}>
-			RightBar
+			<div className={"rightBarWrapper"}>
+				<div className={"birthdayContainer"}>
+					<img className={"birthdayImg"} src={"/assets/gift.png"} alt={"Birthday"}/>
+					<span className={"birthdayText"}>
+						<b>Paul Foster</b> and <b>3 other friends</b> have a birthday today.
+					</span>
+				</div>
+				<img className={"rightBarAd"} src={"/assets/ad.png"} alt={"Ad"}/>
+				<h4 className={"rightBarTitle"}>Online Friends</h4>
+				<ul className={"rightBarFriendList"}>
+					<li className={"rightBarFriend"}>
+						<div className={"rightBarProfileImgContainer"}>
+							<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+							<span className={"rightBarOnline"}></span>
+						</div>
+						<span className={"rightBarUsername"}>John Cart</span>
+					</li>
+					// Removed below items later <br/><br/><br/><br/><br/><br/>
+					<li className={"rightBarFriend"}>
+						<div className={"rightBarProfileImgContainer"}>
+							<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+							<span className={"rightBarOnline"}></span>
+						</div>
+						<span className={"rightBarUsername"}>John Cart</span>
+					</li><li className={"rightBarFriend"}>
+					<div className={"rightBarProfileImgContainer"}>
+						<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+						<span className={"rightBarOnline"}></span>
+					</div>
+					<span className={"rightBarUsername"}>John Cart</span>
+				</li><li className={"rightBarFriend"}>
+					<div className={"rightBarProfileImgContainer"}>
+						<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+						<span className={"rightBarOnline"}></span>
+					</div>
+					<span className={"rightBarUsername"}>John Cart</span>
+				</li><li className={"rightBarFriend"}>
+					<div className={"rightBarProfileImgContainer"}>
+						<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+						<span className={"rightBarOnline"}></span>
+					</div>
+					<span className={"rightBarUsername"}>John Cart</span>
+				</li><li className={"rightBarFriend"}>
+					<div className={"rightBarProfileImgContainer"}>
+						<img className={"rightBarProfileImg"} src={"/assets/person/3.jpeg"} alt={"Profile"}/>
+						<span className={"rightBarOnline"}></span>
+					</div>
+					<span className={"rightBarUsername"}>John Cart</span>
+				</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
