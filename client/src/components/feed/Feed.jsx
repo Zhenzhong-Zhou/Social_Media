7dad98cf92fc681637e893000685493ffc59aade
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 03:36:48 -0600

    Add and implement template data into post component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,16 +1,16 @@
-import "./feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import "./feed.css";
+import {Posts} from "../../templateData";
 
 const Feed = () => {
 	return (
 		<div className={"feed"}>
 			<div className={"feedWrapper"}>
 				<Share/>
-				<Post/>
-				<Post/>
-				<Post/>
-				<Post/>
+				{Posts.map((post) => (
+					<Post key={post.id} post={post}/>
+				))}
 			</div>
 		</div>
 	);
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,33 @@
-import "./post.css";
 import {MoreVert} from "@material-ui/icons";
+import "./post.css";
+import {Users} from "../../templateData";
 
-const Post = () => {
+const Post = ({post}) => {
 	return (
 		<div className={"post"}>
 			<div className={"postWrapper"}>
 				<div className={"postTop"}>
 					<div className={"postTopLeft"}>
-						<img className={"postProfileImg"} src={"/assets/person/1.jpeg"} alt={"Person"}/>
-						<span className={"postUsername"}>Andy</span>
-						<span className={"postDate"}>5 minutes ago</span>
+						<img className={"postProfileImg"} src={Users.filter((user) => user.id === post.userId)[0].profilePicture} alt={"Person"}/>
+						<span className={"postUsername"}>{Users.filter((user) => user.id === post.userId)[0].username}</span>
+						<span className={"postDate"}>{post.date}</span>
 					</div>
 					<div className={"postTopRight"}>
 						<MoreVert/>
 					</div>
 				</div>
 				<div className={"postCenter"}>
-					<span className={"postText"}>First Post:) !</span>
-					<img className={"postImg"} src={"/assets/post/1.jpeg"} alt={"Post"}/>
+					<span className={"postText"}>{post?.desc}</span>
+					<img className={"postImg"} src={post.photo} alt={"Post"}/>
 				</div>
 				<div className={"postBottom"}>
 					<div className={"postBottomLeft"}>
 						<img className={"likeIcon"} src={"/assets/like.png"} alt={"Like"}/>
 						<img className={"likeIcon"} src={"/assets/heart.png"} alt={"Heart"}/>
-						<span className={"postLikeCounter"}>32 people like it</span>
+						<span className={"postLikeCounter"}>{post.like} people like it</span>
 					</div>
 					<div className={"postBottomRight"}>
-						<span className={"postCommentText"}>9 comments</span>
+						<span className={"postCommentText"}>{post.comment} comments</span>
 					</div>
 				</div>
 			</div>
