7cbd5a3bc4e6bb98daa0b724a167844637782096
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 01:33:09 -0600

    Finsihed share component and bug fixes

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -1,9 +1,37 @@
 import "./share.css";
+import {EmojiEmotions, Label, PermMedia, Room} from "@material-ui/icons";
 
 const Share = () => {
 	return (
-		<div>
-			Share
+		<div className={"share"}>
+			<div className={"shareWrapper"}>
+				<div className={"shareTop"}>
+					<img className={"shareProfileImg"} src={"/assets/person/1.jpeg"} alt={"Person"}/>
+					<input placeholder={"What's in your mind Andy?"} className={"shareInput"}/>
+				</div>
+				<hr className={"shareHr"}/>
+				<div className={"shareBottom"}>
+					<div className={"shareOptions"}>
+						<div className={"shareOption"}>
+							<PermMedia htmlColor={"tomato"} className={"shareIcon"}/>
+							<span className={"shareOptionText"}>Photo or Video</span>
+						</div>
+						<div className={"shareOption"}>
+							<Label htmlColor={"blue"} className={"shareIcon"}/>
+							<span className={"shareOptionText"}>Tags</span>
+						</div>
+						<div className={"shareOption"}>
+							<Room htmlColor={"green"} className={"shareIcon"}/>
+							<span className={"shareOptionText"}>Location</span>
+						</div>
+						<div className={"shareOption"}>
+							<EmojiEmotions htmlColor={"goldenrod"} className={"shareIcon"}/>
+							<span className={"shareOptionText"}>Feelings</span>
+						</div>
+					</div>
+					<button className={"shareButton"}>Share</button>
+				</div>
+			</div>
 		</div>
 	);
 };
