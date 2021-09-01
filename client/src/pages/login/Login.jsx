eca584f67775c2c94f2fd73a6198742e5161daab
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-01 00:03:43 -0600

    Add register logic into register component

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -5,14 +5,14 @@ import {loginCall} from "../../actions/apiCalls";
 import {AuthContext} from "../../context/AuthContext";
 
 const Login = () => {
-	const email = useRef()
+	const email = useRef();
 	const password = useRef();
 	const {user, isFetching, error, dispatch} = useContext(AuthContext);
 	const handleClick = (event) => {
 		event.preventDefault();
 		loginCall({email: email.current.value, password: password.current.value}, dispatch);
 	};
-	console.log(user);
+
 	return (
 		<div className={"login"}>
 			<div className={"loginWrapper"}>
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,33 @@
+import {useRef} from "react";
+import {useHistory} from "react-router-dom";
 import "./register.css";
+import {axiosInstance} from "../../api";
 
 const Register = () => {
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const confirmedPassword = useRef();
+	const history = useHistory();
+	const handleClick = async (event) => {
+		event.preventDefault();
+		if (confirmedPassword.current.value !== password.current.value) {
+			confirmedPassword.current.setCustomValidity("Password dose not match!");
+		} else {
+			const user = {
+				username: username.current.value,
+				email: email.current.value,
+				password: password.current.value
+			};
+			try {
+				await axiosInstance.post("auth/register", user);
+				history.push("/login");
+			} catch (error) {
+				console.log(error);
+			}
+		}
+	};
+
 	return (
 		<div className={"login"}>
 			<div className={"loginWrapper"}>
@@ -11,14 +38,14 @@ const Register = () => {
 					</span>
 				</div>
 				<div className={"loginRight"}>
-					<div className={"loginBox"}>
-						<input placeholder={"Username"} className={"loginInput"}/>
-						<input placeholder={"Email"} className={"loginInput"}/>
-						<input placeholder={"Password"} className={"loginInput"}/>
-						<input placeholder={"Confirmed Password"} className={"loginInput"}/>
-						<button className={"loginButton"}>Sign Up</button>
+					<form className={"loginBox"} onSubmit={handleClick}>
+						<input placeholder={"Username"} required ref={username} className={"loginInput"}/>
+						<input placeholder={"Email"} required ref={email} className={"loginInput"} type={"email"}/>
+						<input placeholder={"Password"} required ref={password} className={"loginInput"} type={"password"} minLength={6}/>
+						<input placeholder={"Confirmed Password"} required ref={confirmedPassword} className={"loginInput"} type={"password"} minLength={6}/>
+						<button className={"loginButton"} type={"submit"}>Sign Up</button>
 						<button className={"loginRegisterButton"}>Login your Account</button>
-					</div>
+					</form>
 				</div>
 			</div>
 		</div>
