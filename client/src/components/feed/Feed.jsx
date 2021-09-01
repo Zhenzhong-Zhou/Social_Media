6fedb62d97b42ae0119d4386f562d8fe44011b7c
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-31 19:59:53 -0600

    Feed component initial connection with database

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,16 +1,26 @@
+import {useEffect, useState} from "react";
 import Share from "../share/Share";
 import Post from "../post/Post";
 import "./feed.css";
-import {Posts} from "../../templateData";
+import {axiosInstance} from "../../api";
 
 const Feed = () => {
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		const fetchPost = async () => {
+			const {data} = await axiosInstance.get("posts/timeline/612c3721373f38d180dbbd23");
+			setPosts(data);
+		}
+		fetchPost();
+	}, []);
+
 	return (
 		<div className={"feed"}>
 			<div className={"feedWrapper"}>
 				<Share/>
-				{Posts.map((post) => (
-					<Post key={post.id} post={post}/>
-				))}
+				{/*{posts.map((post) => (*/}
+				{/*	<Post key={post.id} post={post}/>*/}
+				{/*))}*/}
 			</div>
 		</div>
 	);
