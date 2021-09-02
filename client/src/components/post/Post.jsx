d4ecc142fe64ef5b13b7b2e45a7ba27a99bb3b53
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-01 20:39:00 -0600

    Add like and dislike functionality and connect with server in post component

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,15 +1,17 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {MoreVert} from "@material-ui/icons";
 import moment from "moment";
 import {Link} from "react-router-dom";
 import "./post.css";
 import {axiosInstance} from "../../api";
+import {AuthContext} from "../../context/AuthContext";
 
 const Post = ({post}) => {
 	const [like, setLike] = useState(post.likes.length);
 	const [isLiked, setIsLiked] = useState(false);
 	const [user, setUser] = useState({});
 	const assets = process.env.REACT_APP_PUBLIC_FOLDER;
+	const {user: currentUser} = useContext(AuthContext);
 
 	useEffect(() => {
 		const fetchUser = async () => {
@@ -19,7 +21,15 @@ const Post = ({post}) => {
 		fetchUser();
 	}, [post.userId]);
 
+	useEffect(() => {
+		setIsLiked(post.likes.includes(currentUser._id));
+	}, [currentUser._id, post.likes]);
+
 	const likeHandler = () => {
+		try {
+			axiosInstance.put(`posts/${post._id}/like`, {userId: currentUser._id})
+		} catch (error) {}
+
 		setLike(isLiked ? like - 1 : like + 1);
 		setIsLiked(!isLiked);
 	};
