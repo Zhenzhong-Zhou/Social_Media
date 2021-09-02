da086f44df85ddde8df57d7d73e3813eab111599
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-02 02:49:14 -0600

    Add new action types and clean following array

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -2,7 +2,7 @@ import {createContext, useReducer} from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-	user: {"_id":"612c37ac13c2fb01aad9d943","username":"Jobs","email":"Jobs@gamil.com","profilePicture":"person/3.jpeg","coverPicture":"","followers":[],"followings":["612c315aadda93acc6bfb559","612c3721373f38d180dbbd23"],"isAdmin":false},
+	user: {"_id":"612c37ac13c2fb01aad9d943","username":"Jobs","email":"Jobs@gamil.com","profilePicture":"person/3.jpeg","coverPicture":"","followers":[],"followings":[],"isAdmin":false},
 	isFetching: false,
 	error: false
 }
